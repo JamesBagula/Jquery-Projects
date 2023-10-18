a0155a55ca9179db7f3113a44eedb9f970adc69c
JamesBagula <72625441+JamesBagula@users.noreply.github.com>
2023-10-18 13:38:51 +0530

    Add files via upload

---
+++ Google Sign-In Form/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.input-field').focusout(function (){
+
+        var inputValue = $(this).val();
+        if (inputValue === "") {
+            $(this).removeClass("has-value");
+        } else {
+            $(this).addClass("has-value");
+        }
+
+    });
+});
